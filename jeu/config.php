eac52915ab6e8ced71ef29c2603d944aed6baef3
MatthieuLiatard <31052514+MatthieuLiatard@users.noreply.github.com>
2023-07-09 14:59:00 -0400

    Centralisation des variables du jeu
    Report des constantes de contraintes de construction dans config.php

--- jeu/config.php
+++ jeu/config.php
@@ -6,13 +6,31 @@ define ("DUREE_ANTI_ZERK_S", 10*60*60);
 // Connexion a la bdd
 define ("BDD_NAME", "nvs");
 define ("BDD_LOGIN", "root");
-define ("BDD_PASSWORD", "yolo");
-// Brouillard de guerre de 1 jours
-define ("BROUILLARD_DE_GUERRE_S", 24*3600*1);
+define ("BDD_PASSWORD", "");
+// Brouillard de guerre de 2 jours
+define ("BROUILLARD_DE_GUERRE_S", 24*3600*2);
 // Difference nombre de joueurs max pour inscription
 define ("MAX_NB_JOUEUR_DIFF", 3);
 
-// Parametres pour test / version alpha
-//define ("DUREE_TOUR", 5*60);
-//define ("DUREE_ANTI_ZERK_S", 1*60);
+
+/*
+	Parametres afferants aux regles du jeu
+*/
+// CONTRAINTES DE CONSTRUCTION
+// pourraient etre mises en base de donnee...
+// NB genie et docteurs
+define("CONTRAINTES_CONSTRUCTION_FORT_NB_GENIE", 16)
+define("CONTRAINTES_CONSTRUCTION_FORTIN_NB_GENIE", 10)
+define("CONTRAINTES_CONSTRUCTION_GARE_NB_GENIE", 6)
+define("CONTRAINTES_CONSTRUCTION_HOSTO_NB_GENIE", 3)
+define("CONTRAINTES_CONSTRUCTION_TDG_NB_GENIE", 1)
+
+// Distance batiments
+define("CONTRAINTES_CONSTRUCTION_FORT_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0))
+define("CONTRAINTES_CONSTRUCTION_FORTIN_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0))
+define("CONTRAINTES_CONSTRUCTION_GARE_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 40, "TDG" => 0))
+define("CONTRAINTES_CONSTRUCTION_HOSTO_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 20, "TDG" => 0))
+define("CONTRAINTES_CONSTRUCTION_BARRICADE_PONT_DISTANCE", array("Bat" => 0, "Rapat" => 2, "Gare" => 2, "TDG" => 0))
+define("CONTRAINTES_CONSTRUCTION_TDG_DISTANCE", array("Bat" => 2, "Rapat" => 2, "Gare" => 2, "TDG" => 7))
+
 ?>
