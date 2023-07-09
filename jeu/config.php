c49fb6e71550ea73f4619a4c093b2d20a48d20b6
MatthieuLiatard <31052514+MatthieuLiatard@users.noreply.github.com>
2023-07-09 15:18:49 -0400

    Fix config
    Ajout de ; en fin de lignes...

--- jeu/config.php
+++ jeu/config.php
@@ -19,18 +19,18 @@ define ("MAX_NB_JOUEUR_DIFF", 3);
 // CONTRAINTES DE CONSTRUCTION
 // pourraient etre mises en base de donnee...
 // NB genie et docteurs
-define("CONTRAINTES_CONSTRUCTION_FORT_NB_GENIE", 16)
-define("CONTRAINTES_CONSTRUCTION_FORTIN_NB_GENIE", 10)
-define("CONTRAINTES_CONSTRUCTION_GARE_NB_GENIE", 6)
-define("CONTRAINTES_CONSTRUCTION_HOSTO_NB_GENIE", 3)
-define("CONTRAINTES_CONSTRUCTION_TDG_NB_GENIE", 1)
+define("CONTRAINTES_CONSTRUCTION_FORT_NB_GENIE", 16);
+define("CONTRAINTES_CONSTRUCTION_FORTIN_NB_GENIE", 10);
+define("CONTRAINTES_CONSTRUCTION_GARE_NB_GENIE", 6);
+define("CONTRAINTES_CONSTRUCTION_HOSTO_NB_GENIE", 3);
+define("CONTRAINTES_CONSTRUCTION_TDG_NB_GENIE", 1);
 
 // Distance batiments
-define("CONTRAINTES_CONSTRUCTION_FORT_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0))
-define("CONTRAINTES_CONSTRUCTION_FORTIN_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0))
-define("CONTRAINTES_CONSTRUCTION_GARE_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 40, "TDG" => 0))
-define("CONTRAINTES_CONSTRUCTION_HOSTO_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 20, "TDG" => 0))
-define("CONTRAINTES_CONSTRUCTION_BARRICADE_PONT_DISTANCE", array("Bat" => 0, "Rapat" => 2, "Gare" => 2, "TDG" => 0))
-define("CONTRAINTES_CONSTRUCTION_TDG_DISTANCE", array("Bat" => 2, "Rapat" => 2, "Gare" => 2, "TDG" => 7))
+define("CONTRAINTES_CONSTRUCTION_FORT_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0));
+define("CONTRAINTES_CONSTRUCTION_FORTIN_DISTANCE", array("Bat" => 2, "Rapat" => 40, "Gare" => 20, "TDG" => 0));
+define("CONTRAINTES_CONSTRUCTION_GARE_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 40, "TDG" => 0));
+define("CONTRAINTES_CONSTRUCTION_HOSTO_DISTANCE", array("Bat" => 2, "Rapat" => 20, "Gare" => 20, "TDG" => 0));
+define("CONTRAINTES_CONSTRUCTION_BARRICADE_PONT_DISTANCE", array("Bat" => 0, "Rapat" => 2, "Gare" => 2, "TDG" => 0));
+define("CONTRAINTES_CONSTRUCTION_TDG_DISTANCE", array("Bat" => 2, "Rapat" => 2, "Gare" => 2, "TDG" => 7));
 
 ?>
